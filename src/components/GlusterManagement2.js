6a31934bc2166651a48bc4ddb583b82057c94578
Rohan CJ <rojoseph@redhat.com>
2018-06-28 19:20:37 +0530

    wip. good state. only need to fix expando padding and add buttons for expand cluster and create volume

--- src/components/GlusterManagement2.js
+++ src/components/GlusterManagement2.js
@@ -7,7 +7,7 @@ class GlusterManagement2 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      volumeSelectedRow: 'None',
+      selectedVolumes: {},
       hostList: null,
       volumeBricks: null,
       volumeInfo: null,
@@ -15,11 +15,13 @@ class GlusterManagement2 extends Component {
       gdeployState: "",
       gdeployWizardType: ""
     };
-    //Binding "this" to methods to preserve scope
+    //Binding "this" of the component to "this" of the component
     this.updateGlusterInfo = this.updateGlusterInfo.bind(this);
     this.updateHostInfo = this.updateHostInfo.bind(this);
     this.updateVolumeInfo = this.updateVolumeInfo.bind(this);
-    this.getVolumeStatus= this.getVolumeStatus.bind(this);
+    this.updateVolumeStatus= this.updateVolumeStatus.bind(this);
+    this.handleVolumeRowClick= this.handleVolumeRowClick.bind(this);
+
   }
   componentDidMount(){
 
@@ -37,13 +39,13 @@ class GlusterManagement2 extends Component {
     that.updateHostInfo(
       function (hostList){
         commonGlusterState["hostList"] = hostList;
-        that.updateVolumeInfo(
-          function (volumeInfo){
-            commonGlusterState["volumeInfo"] = volumeInfo;
-            that.setState(commonGlusterState);
-          });
+        that.setState({hostList:hostList})
       }
     );
+    that.updateVolumeInfo(
+      function (volumeInfo){
+        that.setState({volumeInfo:volumeInfo});
+    });
 
   }
 
@@ -58,8 +60,8 @@ class GlusterManagement2 extends Component {
     )
     .fail(
       function(err){
-        console.log("Error while fetching volume status: ", err);
-        callback({});
+        console.log("Error while fetching host info: ", err);
+        callback(null);
       }
     );
   }
@@ -77,37 +79,64 @@ class GlusterManagement2 extends Component {
     .fail(
       function(err){
         console.log("Error while fetching volume info: ", err);
-        callback({})
+        callback(null);
       }
     );
   }
 
-  getVolumeStatus(volumeName,callback){
+  updateVolumeStatus(volumeName,callback){
     cockpit.spawn(["vdsm-client", "--gluster-enabled", "GlusterVolume", "status", "volumeName="+volumeName])
     .done(
-      (volumeStatusJson) => callback(JSON.parse(volumeStatusJson))
+      (volumeStatusJson) => callback(JSON.parse(volumeStatusJson).volumeStatus)
     )
     .fail(
       function(err){
-        console.log("Error while fetching volume ",volumeName, " status: ", err);
-        callback({})
+        console.log("Error while fetching volume status for ",volumeName, " status: ", err);
+        callback(null);
       }
     );
   }
 
+  handleVolumeRowClick(volumeName){
+    let that = this;
+    // let newSelectedVolumes = this.state.selectedVolumes;
+    this.setState(function(prevState, props){
+      if(prevState.selectedVolumes.hasOwnProperty(volumeName)){
+        delete prevState.selectedVolumes[volumeName];
+        return {selectedVolumes:prevState.selectedVolumes}
+      }
+      else{
+        prevState.selectedVolumes[volumeName]="fetching";
+        that.updateVolumeStatus(volumeName, function(volumeStatus){
+          that.setState(function(prevState,props){
+            if(prevState.volumeStatus == null){
+              prevState.volumeStatus = {};
+            }
+            prevState.volumeStatus[volumeName]= volumeStatus;
+            prevState.selectedVolumes[volumeName]="";
+            return {volumeStatus:prevState.volumeStatus,selectedVolumes:prevState.selectedVolumes};
+          });
+        });
+
+        return {selectedVolumes:prevState.selectedVolumes}
+      }
+    });
+  }
+
   render(){
+    console.log("hostList:",this.state.hostList);
     return(
       <div>
         <div className="container-fluid">
           <h2 className="title">Gluster Management</h2>
           <div className="row">
             <div className="col-12">
-              {this.state.hostList && <HostsTable hostList={this.state.hostList} />}
+              {this.state.hostList !== null && <HostsTable hostList={this.state.hostList} />}
             </div>
           </div>
           <div className="row">
             <div className="col-12">
-              {this.state.volumeInfo && <VolumeTable volumeInfo={this.state.volumeInfo} />}
+              {this.state.volumeInfo !== null && <VolumeTable volumeInfo={this.state.volumeInfo} selectedVolumes={this.state.selectedVolumes} volumeStatus={this.state.volumeStatus} handleVolumeRowClick={this.handleVolumeRowClick}/>}
             </div>
           </div>
         </div>
@@ -120,15 +149,17 @@ class GlusterManagement2 extends Component {
 class HostsTable extends Component{
   constructor(props){
     super(props);
+  }
+  generateTable(){
     this.hostTableRows = [];
     this.hostTableHeadings =[];
     // for (let heading of Object.keys(props.hostList)){
-    for (let heading of ["Name","Peer status","Uuid"]){
+    for (let heading of ["Name","Peer status","UUID"]){
       this.hostTableHeadings.push(
         <th key={heading}>{heading}</th>
       )
     }
-    for(let host of props.hostList){
+    for(let host of this.props.hostList){
       this.hostTableRows.push(
         <tr key={host.uuid}>
           <td>{host.hostname}</td>
@@ -136,9 +167,10 @@ class HostsTable extends Component{
           <td>{host.uuid}
         </td></tr>);
     }
-
   }
   render(){
+    console.log("Rendering HostsTable");
+    this.generateTable();
     return(
       <div className="panel panel-default">
         <div className="panel-heading">Hosts</div>
@@ -156,25 +188,79 @@ class HostsTable extends Component{
   }
 }
 
+class VolumeBricksTable extends Component{
+  constructor(props){
+    super(props);
+  }
+  generateTable(){
+    this.volumeBricksTableRows = [];
+    for(let brick of this.props.volumeBrickList){
+      this.volumeBricksTableRows.push(
+        <tr key={brick.brick}>
+          <td>{brick.brick}</td>
+          <td>{brick.hostuuid}</td>
+          <td>{brick.status}</td>
+        </tr>);
+    }
+  }
+  render(){
+    this.generateTable()
+    return(
+      <div className="panel panel-default">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Brick</th>
+              <th>Host UUID</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.volumeBricksTableRows}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+
 class VolumeTable extends Component{
   constructor(props){
     super(props);
-    //generate volumeTableRows
+
+  }
+
+  generateTable(){
     this.volumeTableRows = [];
-    for(let volumeName in props.volumeInfo){
-      let volume = props.volumeInfo[volumeName];
+    for(let volumeName in this.props.volumeInfo){
+      let volume = this.props.volumeInfo[volumeName];
+      let expanded = this.props.selectedVolumes.hasOwnProperty(volumeName) && this.props.volumeStatus !== null && this.props.volumeStatus[volumeName] !== undefined;
       this.volumeTableRows.push(
-          <tr key={volume.uuid}>
-            <td className="volume-expando"><span className="fa fa-angle-down volume-expando"></span></td>
-            <td>{volume.volumeName}</td>
+          <tr key={volume.uuid} onClick={()=>{this.props.handleVolumeRowClick(volumeName)}}>
+            <td className="volume-expando">{expanded && <span className="fa fa-angle-down volume-expando"></span>}{!expanded && <span className="fa fa-angle-right volume-expando"></span>}</td>
+            <td>{volumeName}</td>
             <td>{volume.volumeType}</td>
             <td>{volume.volumeStatus}</td>
           </tr>
       );
+      if(expanded){
+        this.volumeTableRows.push(
+          <tr className="no-highlight" key={volumeName+"-brick-status"}>
+            <td className="no-highlight" colSpan="100">
+              <VolumeBricksTable volumeBrickList={this.props.volumeStatus[volumeName].bricks} />
+            </td>
+          </tr>
+        );
+      }
     }
-  }//done generating volumeTableRows
+  }
 
   render(){
+    // console.log("VolumeTable rendered.");
+    // console.log("Volume Status: ",this.props.volumeStatus);
+    // console.log("selectedVolumes: ",this.props.selectedVolumes);
+    this.generateTable();
     return(
       <div className="panel panel-default">
         <div className="panel-heading">Volumes</div>
