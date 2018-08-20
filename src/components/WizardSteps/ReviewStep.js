c329ce49de3cf40900a040e6e6cfeb06f37a59c9
Rohan CJ <rojoseph@redhat.com>
2018-08-20 22:04:06 +0530

    Patternfly-ify Review step.

--- src/components/WizardSteps/ReviewStep.js
+++ src/components/WizardSteps/ReviewStep.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react'
-import {Grid,Row,Col, Form, FormGroup, FormControl, ControlLabel, HelpBlock, Checkbox} from 'patternfly-react'
+import {
+  Grid,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  HelpBlock,
+  Checkbox
+  } from 'patternfly-react'
 import { notEmpty } from '../common/validators'
 import Dropdown from '../common/Dropdown'
 
@@ -7,23 +17,58 @@ import Dropdown from '../common/Dropdown'
 class ReviewStep extends Component {
   constructor(props){
     super(props)
+    this.state = {
+      isEditing: false,
+    }
   }
+  handleEdit = (event) => {
+    this.setState({isEditing:true});
+  }
+  handleSave = (event) => {
+    this.setState({isEditing:false});
+  }
+  handleTextEdit = (event) => {
 
+  }
   render(){
     return (
       <Grid fluid>
         <Row>
-          <Col>{'{ "hosts":'}{JSON.stringify(this.props.glusterModel.hosts)},</Col>
-        </Row>
-        <Row>
-          <Col>"volumes": {JSON.stringify(this.props.glusterModel.volumes)},</Col>
-        </Row>
-        <Row>
-          <Col>"bricks": {JSON.stringify(this.props.glusterModel.bricks)}{"}"}</Col>
-        </Row>
-        <hr/>
-        <Row>
-          <Col>{JSON.stringify(this.props.glusterModel)}</Col>
+          <Col sm={12}>
+            <div className="panel panel-default">
+    <div className="panel-heading">
+        <span className="pficon-settings"></span>
+        <span>
+            Generated Gdeploy configuration : {this.props.configFilePath}
+        </span>
+        <div className="pull-right">
+          {this.state.isEditing &&
+            <button className="btn btn-default"
+              onClick={this.handleSave}>
+              <span className="pficon pficon-save">&nbsp;</span>
+              Save
+            </button>
+          }
+          {!this.state.isEditing &&
+            <button className="btn btn-default"
+                onClick={this.handleEdit}>
+                <span className="pficon pficon-edit">&nbsp;</span>
+                Edit
+            </button>
+          }
+            <button className="btn btn-default"
+              onClick={this.handleReload}>
+              <span className="fa fa-refresh">&nbsp;</span>
+              Reload
+            </button>
+        </div>
+    </div>
+    <textarea className="wizard-preview"
+        value={JSON.stringify(this.props.glusterModel)} onChange={this.handleTextChange} readOnly={!this.state.isEditing}>
+    </textarea>
+</div>
+
+          </Col>
         </Row>
       </Grid>
     );
