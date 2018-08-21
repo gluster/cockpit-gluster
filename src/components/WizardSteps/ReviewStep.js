be95f3043459087d9a1abdeedc7f4f25f328c865
Rohan CJ <rojoseph@redhat.com>
2018-08-21 14:16:54 +0530

    Add yaml dumping

--- src/components/WizardSteps/ReviewStep.js
+++ src/components/WizardSteps/ReviewStep.js
@@ -12,14 +12,34 @@ import {
   } from 'patternfly-react'
 import { notEmpty } from '../common/validators'
 import Dropdown from '../common/Dropdown'
-
+import yaml from 'js-yaml';
 
 class ReviewStep extends Component {
   constructor(props){
     super(props)
     this.state = {
       isEditing: false,
+      inventory: ""
     }
+  }
+
+  nullIfEmpty = (po) => {
+    return JSON.stringify(po) == '{}' ? null : po;
+  }
+  generateInventory = (glusterModel) =>{
+    let groups = {};
+    let { hosts, volumes, bricks, raidConfig, cacheConfig } = glusterModel;
+    groups.hc_nodes.hosts = {};
+    let groupVars ={}
+    for (let hostIndex = 0; hostIndex < hosts.length;hostIndex++){
+      let hostVars = {}
+
+        groups.hc_nodes[hosts[hostIndex]] = nullIfEmpty(hostVars)
+    }
+    groups.hc_nodes.vars = nullIfEmpty(groupVars);
+
+    return yaml.safeDump
+
   }
   handleEdit = (event) => {
     this.setState({isEditing:true});
@@ -27,8 +47,8 @@ class ReviewStep extends Component {
   handleSave = (event) => {
     this.setState({isEditing:false});
   }
-  handleTextEdit = (event) => {
-
+  handleTextChange = (event) => {
+    this.setState({inventory: event.target.value})
   }
   render(){
     return (
